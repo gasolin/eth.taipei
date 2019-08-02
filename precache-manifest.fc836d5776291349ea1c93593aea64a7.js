d4b9d446d4654f224aeadb35af0c8dedb9c901b4
gasolin <gasolin@gmail.com>
2019-08-02 21:39:56 +0800

    Updates

---
+++ precache-manifest.fc836d5776291349ea1c93593aea64a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf2343fc8c8027fe79466e3196a28f7",
+    "url": "/eth.taipei/index.html"
+  },
+  {
+    "revision": "b8b979682211538f719b",
+    "url": "/eth.taipei/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "39b2b1622ebc97c9a6a6",
+    "url": "/eth.taipei/static/css/main.02e31efa.chunk.css"
+  },
+  {
+    "revision": "b8b979682211538f719b",
+    "url": "/eth.taipei/static/js/2.3ce1b247.chunk.js"
+  },
+  {
+    "revision": "39b2b1622ebc97c9a6a6",
+    "url": "/eth.taipei/static/js/main.5a380cd0.chunk.js"
+  },
+  {
+    "revision": "65aba234cbb94e4df9f9",
+    "url": "/eth.taipei/static/js/runtime~main.08b3d580.js"
+  },
+  {
+    "revision": "c67f6de1b8afc128bf2c00195650189a",
+    "url": "/eth.taipei/static/media/background.c67f6de1.png"
+  },
+  {
+    "revision": "c50f665ea4a160608584833cbcc03964",
+    "url": "/eth.taipei/static/media/logo.c50f665e.png"
+  }
+]);
